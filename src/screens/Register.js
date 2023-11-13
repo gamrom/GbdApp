7f9cbb84b0b2812f44c1b7cf933e231983bb6829
김은식 <gamrom@MacBook-Pro.local>
2023-11-14 00:30:48 +0900

    firebase 설정 중 이상해짐

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -3,6 +3,7 @@ import { View, Text } from 'react-native';
 import { TextInput, Button, } from 'react-native-paper';
 import { useState } from 'react';
 import tw from 'twrnc';
+import auth from '@react-native-firebase/auth';
 
 export const Register = ({ navigation }) => {
   const [email, setEmail] = useState('');
@@ -10,6 +11,29 @@ export const Register = ({ navigation }) => {
   const [pwConfirm, setPwConfirm] = useState('');
   const [isSecureA, setIsSecureA] = useState(true);
   const [isSecureB, setIsSecureB] = useState(true);
+
+  const tryRegister = () => {
+    if (pw !== pwConfirm) {
+      alert('비밀번호가 일치하지 않습니다.');
+      return;
+    }
+    auth().createUserWithEmailAndPassword(email, pw)
+      .then(() => {
+        alert('회원가입이 완료되었습니다.');
+        navigation.navigate('RegisterWait');
+      })
+      .catch(error => {
+        if (error.code === 'auth/email-already-in-use') {
+          alert('이미 사용중인 이메일입니다.');
+        }
+        if (error.code === 'auth/invalid-email') {
+          alert('유효하지 않은 이메일입니다.');
+        }
+        if (error.code === 'auth/weak-password') {
+          alert('비밀번호는 6자리 이상이어야 합니다.');
+        }
+      })
+  }
 
   return (
     <View style={tw`flex justify-center h-full bg-white px-[15px]`}>
@@ -34,13 +58,17 @@ export const Register = ({ navigation }) => {
         label="비밀번호 확인"
         value={pwConfirm}
         secureTextEntry={isSecureB}
-        onChangeText={pw => setPw(pwConfirm)}
+        onChangeText={pwConfirm => setPwConfirm(pwConfirm)}
         right={<TextInput.Icon icon="eye"
           onPress={() => setIsSecureB(!isSecureB)}
         />}
       />
 
-      <Button style={tw`mt-4`} mode='elevated' onPress={() => navigation.navigate('RegisterWait')}>회원가입 하기</Button>
+      <Button style={tw`mt-4`} mode='elevated' onPress={
+        () => {
+          tryRegister();
+        }
+      }>회원가입 하기</Button>
     </View>
   );
 }
